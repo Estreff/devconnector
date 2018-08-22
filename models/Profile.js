acc81fc56474f097072686556aab47bc618409c0
Erik Streff <Estreff@gmail.com>
2018-08-21 21:12:51 -0600

    update validation for login and register

---
+++ models/Profile.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+// Create Profile Schema
+const ProfileSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    handle: {
+        type: String,
+        required: true,
+        max: 40
+    },
+    company: {
+        type: String
+    },
+    webiste: {
+        type: String
+    },
+    location: {
+        type: String
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    skills: {
+        type: [String],
+        required: true
+    }
+
+})
--- routes/api/users.js
+++ routes/api/users.js
@@ -4,9 +4,12 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
-
+const passport = require('passport');
 // Load User model
 const User = require('../../models/User');
+// Load Register Input Validation
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // @route   GET api/users/test
 // @desc    Test users route
@@ -15,18 +18,29 @@ router.get('/test', (req, res) => res.json({
     msg: 'User Route Works'
 }));
 
-// @route   GET api/users/register
+// @route   POST api/users/register
 // @desc    Register user
 // @access  Public
 router.post('/register', (req, res) => {
+    const {
+        errors,
+        isValid
+    } = validateRegisterInput(req.body);
+
+    // Validation Check
+    if (!isValid) {
+        return res.status(400).json({
+            errors
+        });
+    }
+
     User.findOne({
             email: req.body.email
         })
         .then(users => {
             if (users) {
-                return res.status(400).json({
-                    email: 'Email already exists'
-                });
+                errors.email = 'Email already exists';
+                return res.status(400).json(errors);
             } else {
                 const avatar = gravatar.url(req.body.email, {
                     s: '200', // Size
@@ -52,10 +66,22 @@ router.post('/register', (req, res) => {
         })
 });
 
-// @route   GET api/users/login
+// @route   POST api/users/login
 // @desc    Login User / Returning JWT Token
 // @access  Public
 router.post('/login', (req, res) => {
+    const {
+        errors,
+        isValid
+    } = validateLoginInput(req.body);
+
+    // Validation Check
+    if (!isValid) {
+        return res.status(400).json({
+            errors
+        });
+    }
+
     const email = req.body.email;
     const password = req.body.password;
 
@@ -66,9 +92,8 @@ router.post('/login', (req, res) => {
         .then(user => {
             // Check for User
             if (!user) {
-                return res.status(404).json({
-                    email: 'Email not found'
-                });
+                errors.email = 'Email not found';
+                return res.status(404).json(errors);
             }
 
             // Check Password
@@ -95,13 +120,26 @@ router.post('/login', (req, res) => {
                             }
                         );
                     } else {
-                        res.status(400).json({
-                            password: 'Incorrect Password'
-                        });
+                        errors.password = 'Incorrect Password';
+                        res.status(400).json(errors);
                     }
                 })
         });
 });
 
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+
+router.get('/current', passport.authenticate('jwt', {
+    session: false
+}), (req, res) => {
+    res.json({
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email,
+        avatar: req.user.avatar
+    });
+});
 
 module.exports = router;
